6bf7f2cb0522ead7c093f5d56837c13913bf01a0
Anish Laddha <anishladdha03@gmail.com>
2020-05-02 19:25:41 -0700

    completely changed, consider V2,  see read me for more info basically added metadata

--- spotifytomp3.py
+++ spotifytomp3.py
@@ -1,120 +1,171 @@
 import spotipy
-import sys
+import os
 import json
 import pprint
-import requests
-import os
-import urllib.request
 from youtube_search import YoutubeSearch
-import fnmatch
-
-
-
+import youtube_dl
+import eyed3
+from urllib.request import urlopen
+import os.path
+import click
 from spotipy.oauth2 import SpotifyClientCredentials
 
 
 
 
-client_credentials_manager = SpotifyClientCredentials(client_id='YOUR CLIENT ID',
-                                                      client_secret='YOUR CLIENT SECRET')
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-pp = pprint.PrettyPrinter(indent=4)
-
-if len(sys.argv) > 1:
-    usr = ' '.join(sys.argv[1:])
-
-else:
-    usr = ''
-
-##sp.trace = True # turn on tracing
-##sp.trace_out = True # turn on trace out
-
-user = sp.user(usr)
-
-playlists = sp.user_playlists(usr)
-
-print("Hello " + user['display_name']+" !")
-n = playlists['items']
-print("your playlists: ")
-for i in n:
-	print(i['name'])
-usrpl = input("Choose your playlist: ")
-for i in n:
-	if i['name'] == usrpl:
-		pluri = i['uri']
-##print(pluri)
-results = sp.playlist_tracks(pluri)
-##pp.pprint(results)
-tracklist = []
-
-filename = usrpl+" list"+".txt"
-filewrite = open(filename, "a")
-fileread = open(filename, "r")
-
-oldlist = []
-
-for x in fileread:
-	oldlist.append(x)
-
-tracks = results['items']
-for i in tracks:
-	string = ""
-	# thingy = i['track']
-	# pp.pprint(thingy['id'])
-	track = i['track']
-	string = string + track['name']+" by "
-	##print(track['name'] + " by ")
-	artists = track['artists']
-	for j in artists:
-		artist = j['name']
-		string = string +artist+" "
-	print(string)
-	tracklist.append(string)
-##print(tracklist)
+class Song:
+    def __init__(self, sname, sartist, coverart, albname, albartist, spotid):
+        self.sname = sname
+        self.sartist = sartist
+        self.coverart = coverart
+        self.albname = albname
+        self.albartist = albartist
+        self.spotid = spotid
+    
+    def encode(self):
+        return self.__dict__
+    
 
 
+def createDict(tdict):
+    tracklist = []
+    for i in tdict:
+        track = i['track']
+        album = track['album']
+        albumname = album["name"] ##album name 
+        albartists = []
+        for i in album["artists"]:
+            albartists.append(i["name"]) ##album artists
+        albimgs = album["images"]
+        try:
+            albimgt = albimgs[0]
+            albimg = albimgt["url"] ## coverart
+        
+        except:
+            albimg = "https://image.shutterstock.com/image-photo/concert-hall-lit-stage-people-600w-1043460649.jpg"
+        
+        songname = track["name"] ##songname
+        spotid = track["id"] ##spotify id
+        artistlist = []
+        
+        for i in track["artists"]:
+            artistlist.append(i["name"]) ##song artists
+        newsong = Song(songname, artistlist, albimg, albumname, albartists, spotid)
+        tracklist.append(newsong)
+    return tracklist
+    ## jsontracks = json.dumps(tracklist, default=lambda o: o.encode(), indent=4)
 
 
-def downloadaudio(url, title):
-	command = "youtube-dl -f m4a "+url
-	##print(command)
-	os.system(command)
+def downloader(url, name):
+    ydl_opts = {
+        'outtmpl': '',
+        'format': 'm4a',
+    }
+    ydl_opts['outtmpl']= name + ".%(ext)s"
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    
 
-failed = []
+def convertor(name):
+    os.system('ftransc -r -q insane -f mp3 '+'"'+name+'.m4a'+'"')
+    
 
-##print(oldlist)
+def tagger(songdata, file):
 
-for i in tracklist:
-	e = i + "\n"
-	if e not in oldlist:
-		filewrite.write(i+"\n")
-		searchresults = YoutubeSearch(i+" audio" , max_results=1).to_dict()
-		#print(searchresults)
-		try:
-			print("trying to download")
-			searchresult = searchresults[0]
-			searchid = searchresult['id']
-			if searchid[0] == "-":
-				searchid = searchid[1:]
-			downloadaudio(searchid, i)
-		except:
-			failed.append(i)
+    songname = songdata.sname
+
+    songartlist = songdata.sartist
+    songartstr = ', '.join(songartlist)
+
+    albumname = songdata.albname
+
+    albartlist = songdata.albartist
+    albartstr = ', '.join(albartlist)
+
+    coverarturl = songdata.coverart
+    
+    audiofile = eyed3.load(file)
+
+    audiofile.tag.artist = songartstr
+    audiofile.tag.album = albumname
+    audiofile.tag.album_artist = albartstr
+    audiofile.tag.title = songname
+
+    response = urlopen(coverarturl)
+    imagedata = response.read()
+
+    audiofile.tag.images.remove(u'')
+    audiofile.tag.images.set(3, imagedata , "image/jpeg" ,u"Description")
+
+    audiofile.tag.save()
+
+
+def youtubelink(song, artist):
+    i = song +" by "+ artist
+    searchresults = YoutubeSearch(i+" audio" , max_results=1).to_dict()
+    searchresult = searchresults[0]
+    searchid = searchresult['id']
+    if searchid[0] == "-":
+        searchid = searchid[1:]
+    return searchid
+
+
+def songexists(song):
+    if os.path.isfile(song+".mp3"):
+        return True
+    else:
+        return False
+
+
+
+@click.command()
+@click.option('--username', prompt='Your username',
+              help='your spotify username')
+@click.option('--playlist', prompt='Your public spotify playlist',
+              help='The public playlist you want to download')
+
+def run(username, playlist):
+    client_credentials_manager = SpotifyClientCredentials(client_id='2528e9e7f4fa492aaecfc9e07c18f444',
+                                                      client_secret='c1595dd6fe0a406ca075ba7e5733ec79')
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    pp = pprint.PrettyPrinter(indent=4)
+
+    usr = username
+    if usr[0] == "@":
+        usr = usr[1:]
+
+
+
+    user = sp.user(usr)
+
+    playlists = sp.user_playlists(usr)
+
+    n = playlists['items']
+
+    usrpl = playlist
+    for i in n:
+    	if i['name'] == usrpl:
+    		pluri = i['uri']
+
+    results = sp.playlist_tracks(pluri)
+    tracks = results['items']
+    tracklist = createDict(tracks) ##creates the list of songs, works fine
+    
+    for track in tracklist:
+        print(track)
+        song = track.sname
+        if songexists(song) == False:
+            artistlist = track.sartist
+            artiststr = ', '.join(artistlist)
+            link = youtubelink(song, artiststr)
+            downloader(link, song)
+            convertor(song)
+            tagger(track, song+".mp3")
+
+
+if __name__ == "__main__":
+    run()
 
 
 
 
-
-
-print(failed)
-
-#wc7JPaRV5uU
-
-m4as = []
-for root, dirs, files in os.walk(os.getcwd()):
-    m4as += fnmatch.filter(files, '*.m4a')
-##print(m4as)
-count = 0
-for i in m4as:
-	print(i+ " "+str(count))
-	count= count +1
-	os.system("ftransc -r -q extreme -f mp3 "+"'"+i+"'")
